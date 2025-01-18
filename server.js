e8c8edeccae21fc7c2474dcb8a6bbf2482cf3b91
Lord9s <nkunajunior399@gmail.com>
2025-01-18 16:07:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-// server.js
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/', (req, res) => {
